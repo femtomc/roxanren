c7c700f1b1f97a0e7a1e24bbaf1082d7421c57af
femtomc <mccoybecker@gmail.com>
2021-09-14 22:22:30 -0400

    Update.

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "roxanren")
-(define deps '("base"))
+(define deps '("base" "rosette"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/roxanren.scrbl" ())))
 (define pkg-desc "An experimental fusion of miniKanren + virtualized access to SMT solvers (Rosette).")
